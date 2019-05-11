9e1a109b7818bc288a6413ad704027917d722488
Omar <35585097+GrawellDev@users.noreply.github.com>
2019-05-11 14:48:24 -0500

    Update onDeath.java

--- src/main/java/net/fixer/firstplugin/events/onDeath.java
+++ src/main/java/net/fixer/firstplugin/events/onDeath.java
@@ -10,7 +10,7 @@ public class onDeath implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e){
-        e.getEntity().sendMessage(ChatColor.AQUA + "Thank you for playing a UHC provided by the Badlion Network!");
+        e.getEntity().sendMessage(ChatColor.AQUA + "Thank you for playing a UHC provided by the Fixer Network!");
         e.getEntity().sendMessage(ChatColor.AQUA + "You can now spectate other players but will be unable to interact with the game");
         e.getEntity().setGameMode(GameMode.CREATIVE);
         e.getEntity().setFlying(true);
